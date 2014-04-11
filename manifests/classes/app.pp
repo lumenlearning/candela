ca1ae2177bfdd71585422bfe614aa07cc935d29f
Jeff Graham <jeff@funnymonkey.com>
2014-04-10 17:08:15 -0700

    Setup dependencies for pressbooks

--- manifests/classes/app.pp
+++ manifests/classes/app.pp
@@ -23,4 +23,38 @@ class app {
     table       => 'wordpress.*',
     user        => 'wordpress@localhost'
   }
+
+
+  # Setup Pressbooks dependencies
+  # xmllint
+  package { libxml2-utils:
+    ensure => installed,
+  }
+
+  # install epubcheck
+  exec { 'install epubcheck':
+    command => '/usr/bin/wget -O /home/vagrant/epubcheck.zip https://github.com/IDPF/epubcheck/releases/download/v3.0.1/epubcheck-3.0.1.zip && unzip /home/vagrant/epubcheck.zip -d /home/vagrant',
+    require => Package['unzip'],
+    creates => '/home/vagrant/epubcheck-3.0.1/epubcheck-3.0.1.jar',
+  }
+
+  # install kindlegen
+  exec { 'install kindlegen':
+  command => 'mkdir /home/vagrant/kindlegen && /usr/bin/wget -O /home/vagrant/kindlegen/kindlegen.tar.gz http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz && tar --directory=/home/vagrant/kindlegen -xvzf /home/vagrant/kindlegen/kindlegen.tar.gz',
+  creates => '/home/vagrant/kindlegen/kindlegen',
+  }
+
+  # install prince
+  package { libtiff4:
+    ensure => installed
+  }
+  package { libgif4:
+    ensure => installed
+  }
+  exec { 'install prince':
+    command => '/usr/bin/wget -O /home/vagrant/prince.deb http://www.princexml.com/download/prince_9.0-4_ubuntu12.04_amd64.deb && sudo dpkg -i /home/vagrant/prince.deb',
+    require => Package['libgif4', 'libtiff4'],
+    creates => '/usr/bin/prince',
+  }
+
 }
