6eeb39e994407d73d6045dc93a1ccfe401413d05
David Lippman <drlippman@yahoo.com>
2014-08-08 20:40:08 -0700

    Add new theme options to Candela theme: 1) Collapsible table of contents 2) Adding special styles based on the content source 3) Option to enable MathJax

--- wp-content/plugins/candela-utility/themes/candela/functions.php
+++ wp-content/plugins/candela-utility/themes/candela/functions.php
@@ -12,3 +12,173 @@ function fitzgerald_enqueue_styles() {
 	wp_enqueue_style( 'fitzgerald-fonts', 'http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,700|Roboto+Condensed:400,300,300italic,400italic' );
 }
 add_action( 'wp_print_styles', 'fitzgerald_enqueue_styles' );
+
+/* ------------------------------------------------------------------------ *
+ * Global Options Tab
+ * ------------------------------------------------------------------------ */
+
+// Global Options Registration
+function candela_theme_options_global_init() {
+
+	$_page = 'pressbooks_theme_options_global';
+	$_option = 'candela_theme_options_global';
+	$_section = 'global_options_section';
+	$defaults = array(
+		'toc_collapse' => 0
+	);
+
+	if ( false == get_option( $_option ) ) {
+		add_option( $_option, $defaults );
+	}
+
+	add_settings_field(
+		'toc_collapse',
+		__( 'Table of Contents', 'candela' ),
+		'candela_theme_toc_collapse_callback',
+		$_page,
+		$_section,
+		array(
+			 __( 'Make Table of Contents Collapsible', 'candela' )
+		)
+	);
+	
+	add_settings_field(
+		'source_based_css',
+		__( 'Load source-based styles', 'candela' ),
+		'candela_theme_source_based_css_callback',
+		$_page,
+		$_section,
+		array(
+			 __( 'OpenStax', 'candela' ),
+			 __( 'Lardbucket', 'candela' )
+		)
+	);
+	
+	add_settings_field(
+		'enable_mathjax',
+		__( 'MathJax', 'candela' ),
+		'candela_theme_mathjax_callback',
+		$_page,
+		$_section,
+		array(
+			 __( 'Enable MathJax', 'candela' )
+		)
+	);
+	
+	register_setting(
+		$_page,
+		$_option,
+		'candela_theme_options_global_sanitize'
+	);
+}
+add_action('admin_init', 'candela_theme_options_global_init');
+
+
+// TOC Options Field Callback
+function candela_theme_toc_collapse_callback( $args ) {
+
+	$options = get_option( 'candela_theme_options_global' );
+	
+	if ( ! isset( $options['toc_collapse'] ) ) {
+		$options['toc_collapse'] = 0;
+	}
+
+	$html = '<input type="checkbox" id="toc_collapse" name="candela_theme_options_global[toc_collapse]" value="1" ' . checked( 1, $options['toc_collapse'], false ) . '/>';
+	$html .= '<label for="toc_collapse"> ' . $args[0] . '</label>';
+	echo $html;
+}
+
+// Source based CSS Field Callback
+function candela_theme_source_based_css_callback( $args ) {
+
+	$options = get_option( 'candela_theme_options_global' );
+	
+	if ( ! isset( $options['source_based_css'] ) ) {
+		$options['source_based_css'] = 0;
+	}
+	$html .= '<input type="checkbox" id="source_based_css_1" name="candela_theme_options_global[source_based_css_1]" value="1" '.checked(1, $options['source_based_css']&1, false) . '/>';
+	$html .= '<label for="source_based_css_1">'.$args[0].'</label><br/>';
+	$html .= '<input type="checkbox" id="source_based_css_2" name="candela_theme_options_global[source_based_css_2]" value="2" '.checked(2, $options['source_based_css']&2, false) . '/>';
+	$html .= '<label for="source_based_css_2">'.$args[1].'</label>';
+	
+	echo $html;
+}
+
+// Mathjax Field Callback
+function candela_theme_mathjax_callback( $args ) {
+
+	$options = get_option( 'candela_theme_options_global' );
+	
+	if ( ! isset( $options['enable_mathjax'] ) ) {
+		$options['enable_mathjax'] = 0;
+	}
+	$html .= '<input type="checkbox" id="enable_mathjax" name="candela_theme_options_global[enable_mathjax]" value="1" '.checked(1, $options['enable_mathjax'], false) . '/>';
+	$html .= '<label for="enable_mathjax">'.$args[0].'</label>';
+	
+	echo $html;
+}
+
+// Global Options Input Sanitization
+function candela_theme_options_global_sanitize( $input ) {
+
+	$options = get_option( 'candela_theme_options_global' );
+
+	if ( ! isset( $input['toc_collapse'] ) || $input['toc_collapse'] != '1' ) {
+		$options['toc_collapse'] = 0;
+	} else {
+		$options['toc_collapse'] = 1;
+	}
+	
+	$options['source_based_css'] = 0;
+	if ( isset( $input['source_based_css_1'] ) && $input['source_based_css_1']=='1' ) {
+		$options['source_based_css'] += 1;
+	} 
+	if ( isset( $input['source_based_css_2'] ) && $input['source_based_css_2']=='2' ) {
+		$options['source_based_css'] += 2;
+	} 
+	
+	if ( ! isset( $input['enable_mathjax'] ) || $input['enable_mathjax'] != '1' ) {
+		$options['enable_mathjax'] = 0;
+	} else {
+		$options['enable_mathjax'] = 1;
+	}
+	return $options;
+}
+
+
+/**
+ * Get any header scripts, based on options
+ *
+ * @return string
+ *
+ */
+function candela_get_header_scripts() {
+	$options = get_option( 'candela_theme_options_global' );
+	if ( @$options['toc_collapse'] ) {
+		wp_enqueue_script(
+			'candela_toc_collapse', 
+			get_stylesheet_directory_uri().'/js/toc_collapse.js',
+			array( 'jquery' ));
+		wp_enqueue_style( 'dashicons' );
+	}
+	if ((@$options['source_based_css']&1)==1) {
+		wp_enqueue_style('candela_openstax_css', 
+			get_stylesheet_directory_uri().'/css/openstax.css');
+		wp_enqueue_script(
+			'candela_openstax_js', 
+			get_stylesheet_directory_uri().'/js/openstax.js',
+			array( 'jquery' ));
+	}
+	if ((@$options['source_based_css']&2)==2) {
+		wp_enqueue_style('candela_lb_css', 
+			get_stylesheet_directory_uri().'/css/lb.css');
+	}
+	if ( @$options['enable_mathjax'] ) {
+		wp_enqueue_script(
+			'candela_enable_mathjax', 
+			'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML');
+	}
+}
+add_action('wp_enqueue_scripts', 'candela_get_header_scripts');
+
+?>
