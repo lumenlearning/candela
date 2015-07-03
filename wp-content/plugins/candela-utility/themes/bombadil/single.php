7cfa0c6591ffe8e4afcc0565f22adef2f5d56379
Bryan <bryan@lumenlearning.com>
2015-07-02 17:30:52 -0700

    implement most of the styling needed for study plan pages and add logic to determine what kind of page (study plan or content page) to load

--- wp-content/plugins/candela-utility/themes/bombadil/single.php
+++ wp-content/plugins/candela-utility/themes/bombadil/single.php
@@ -1,54 +1,18 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<?php get_header(); ?>
-<?php if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))): ?>
+	<?php get_header(); ?>
+	<?php if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))): ?>
 
-			<main id="main-content">
+		<?php
+			if (get_post_type($post->ID) !== 'part') {
+				include('single_page.php');
+			}
+			else {
+				include('single_study_plan.php');
+			}
+		?>
 
-			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-
-				<div id="post-<?php the_ID(); ?>" <?php post_class( pb_get_section_type( $post ) ); ?>>
-
-					<div class="entry-content">
-
-						<?php
-						the_content();
-						if ( get_post_type( $post->ID ) === 'part' ) {
-							echo get_post_meta( $post->ID, 'pb_part_content', true );
-						} ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-## -->
-
-				<?php if ( $citation = CandelaCitation::renderCitation( $post->ID ) ): ?>
-					<section role="contentinfo">
-						<div class="post-citations sidebar">
-							<div role="button" aria-pressed="false" id="citation-header-<?php print $post->ID; ?>" class="collapsed h3-styling"><?php _e('Licenses and Attributions'); ?></div>
-							<div id="citation-list-<?php print $post->ID; ?>" style="display:none;">
-								<?php print $citation ?>
-							</div>
-							<script>
-								jQuery( document ).ready( function( $ ) {
-									var pressed = false;
-									$( "#citation-header-<?php print $post->ID;?>" ).click(function() {
-										pressed = !pressed;
-										$( "#citation-list-<?php print $post->ID;?>" ).slideToggle();
-										$( "#citation-header-<?php print $post->ID;?>" ).toggleClass('expanded collapsed');
-										$( "#citation-header-<?php print $post->ID;?>" ).attr('aria-pressed', pressed);
-									});
-								});
-							</script>
-						</div>
-
-					</section>
-				<?php endif; ?>
-
-				<!-- previous/next page links -->
-				<?php ca_get_links(); ?>
-
-				</div><!-- #content -->
-
-				<?php comments_template( '', true ); ?>
-<?php else: ?>
-<?php pb_private(); ?>
-<?php endif; ?>
-<?php get_footer(); ?>
+	<?php else: ?>
+		<?php pb_private(); ?>
+	<?php endif; ?>
+	<?php get_footer(); ?>
 <?php endwhile;?>
