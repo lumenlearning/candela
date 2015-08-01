29bc49486640c2b839e07ed32273764675f8bc8b
Bracken Mosbacker <bracken@lumenlearning.com>
2015-08-01 14:04:37 -0700

    update embed code for lumen assessments
    Test Plan:
     * putting an assessments.lumenlearning.com url in a page
       should make it embed with the proper styles and
    	 functionality

--- wp-content/plugins/candela-utility/candela-utility.php
+++ wp-content/plugins/candela-utility/candela-utility.php
@@ -240,10 +240,11 @@ function register_oembed_providers() {
 		'oea.herokuapp.com' => array(
 			'regex' => '#https?://(oea\.herokuapp\.com)/assessments/(.*)#i',
 		),
-		'assessments.lumenlearning.com' => array(
-			'regex' => '#https?://(assessments\.lumenlearning\.com)/assessments/(.*)#i',
-		),
 	);
+
+  wp_embed_register_handler( 'assessments.lumenlearning.com',
+      '#https?://assessments\.lumenlearning\.com/assessments/(.*)#i',
+      '\Candela\Utility\lumen_asmnt_embed_handler' );
 
 	foreach ($providers as $id => $info ) {
 		wp_embed_register_handler( $id, $info['regex'], '\Candela\Utility\embed_handler' );
@@ -276,6 +277,36 @@ function embed_handler( $matches, $attr, $url, $rawattr ) {
 		esc_attr($matches[2]),
 		$parms
 	);
+
+	return apply_filters( 'embed_oea', $embed, $matches, $attr, $url, $rawattr );
+}
+
+function lumen_asmnt_embed_handler( $matches, $attr, $url, $rawattr ) {
+  $assessment_id = esc_attr($matches[1]);
+
+  switch_to_blog(1);
+  $external_id = get_user_meta( wp_get_current_user()->ID, 'candelalti_external_userid', true );
+  restore_current_blog();
+
+	$parameters = array(
+      sprintf('src_url=https://assessments.lumenlearning.com/api/assessments/%d.xml', $assessment_id),
+      sprintf('assessment_id=%d', $assessment_id),
+      'results_end_point=https://assessments.lumenlearning.com/api',
+      'confidence_levels=true',
+      'enable_start=true',
+      'style=lumen_learning',
+      'assessment_kind=formative',
+      'external_user_id=' . esc_attr($external_id),
+      sprintf('iframe_resize_id=lumen_assessment_%d', $assessment_id),
+  );
+
+	$params = implode('&', $parameters);
+
+  $iframe = <<<HTML
+  <iframe id="lumen_assessment_%d" class="resizable" src="https://assessments.lumenlearning.com/assessments/load?%s"
+  frameborder="0" style="border:none;width:100%%;height:100%%;min-height:575px;"></iframe>
+HTML;
+	$embed = sprintf( $iframe, $assessment_id, $params);
 
 	return apply_filters( 'embed_oea', $embed, $matches, $attr, $url, $rawattr );
 }
