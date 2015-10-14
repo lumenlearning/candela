f8047d2654db10865d6a24a82d9f8d0d22a8572b
monkecheese <bryan.borgeson@gmail.com>
2015-10-14 14:53:09 -0700

    clean up spacing in manifest.php for human readability

--- wp-content/plugins/candela-thin-exports/cc/manifest.php
+++ wp-content/plugins/candela-thin-exports/cc/manifest.php
@@ -38,32 +38,31 @@ class Manifest extends Base
   public static $available_options = ['inline', 'include_fm', 'include_bm', 'export_flagged_only',
                                       'use_custom_vars', 'include_parts', 'include_guids'];
 
-  public function __construct($structure, $options=[])
-  {
+  public function __construct($structure, $options=[]) {
     $this->book_structure = $structure;
     $this->version = isset($options['version']) ? $options['version'] : 'thin';
     $this->manifest = self::get_manifest_template();
     $this->link_template = $this->get_lti_link_template();
-    if( isset($options['use_page_name_launch_url']) && $options['use_page_name_launch_url']){
+    if (isset($options['use_page_name_launch_url']) && $options['use_page_name_launch_url']) {
       $this->use_page_name_launch_url = true;
     }
     $this->options = $options;
   }
 
-  public function build_manifest(){
+  public function build_manifest() {
     $this->manifest = str_replace('{course_name}', get_bloginfo('name'), $this->manifest);
     $this->manifest = str_replace('{course_description}', get_bloginfo('description'), $this->manifest);
     $this->manifest = str_replace('{organization_items}', $this->item_parts(), $this->manifest);
     $this->manifest = str_replace('{resources}', $this->item_resources(), $this->manifest);
   }
 
-  public function build_zip(){
+  public function build_zip() {
     $this->tmp_file = tempnam("tmp","thincc");
     $zip = new \ZipArchive();
     $zip->open($this->tmp_file,\ZipArchive::OVERWRITE);
 
     $zip->addFromString('imsmanifest.xml', $this->manifest);
-    if(!$this->options['inline']) {
+    if (!$this->options['inline']) {
       $this->add_lti_link_files($zip);
     }
 
@@ -72,17 +71,17 @@ class Manifest extends Base
     return $this->tmp_file;
   }
 
-  public function cleanup(){
+  public function cleanup() {
     if( isset($this->tmp_file) && get_resource_type($this->tmp_file) === 'file' ){
       unlink($this->tmp_file);
     }
   }
 
-  function __destruct(){
+  function __destruct() {
     $this->cleanup();
   }
 
-  private function item_parts(){
+  private function item_parts() {
     $items = '';
     $template = <<<XML
 
@@ -94,15 +93,14 @@ XML;
 
     foreach ($this->book_structure['part'] as $part) {
       $item_pages = $this->item_pages($part);
-      if($item_pages != ''){
+      if ($item_pages != '') {
         $items .= sprintf($template, $this->identifier($part, "IM_"), $part['post_title'], $item_pages);
       }
     }
-
     return $items;
   }
 
-  private function item_pages($part){
+  private function item_pages($part) {
     $items = '';
     $template = <<<XML
 
@@ -112,48 +110,50 @@ XML;
 XML;
 
     // The part link comes first
-    if($this->options['include_parts']) {
+    if ($this->options['include_parts']) {
       $items .= sprintf($template, $this->identifier($part, "I_"), $this->identifier($part), $part['post_title']);
     }
 
     // Then each of the pages
     foreach ($part['chapters'] as $chapter) {
-      if($this->export_page($chapter)) {
+      if ($this->export_page($chapter)) {
         $items .= sprintf($template, $this->identifier($chapter, "I_"), $this->identifier($chapter), $chapter['post_title']);
       }
     }
-
     return $items;
   }
 
-  private function item_resources(){
+  private function item_resources() {
     return $this->lti_resources();
   }
 
-  private function get_base_url(){
+  private function get_base_url() {
     $blog_id = get_current_blog_id();
-    if( $this->options['use_custom_vars'] ){
+    if($this->options['use_custom_vars']) {
       return get_site_url(1) . '/api/lti/' . $blog_id;
-    } else if ( $this->use_page_name_launch_url ) {
+    }
+    else if ($this->use_page_name_launch_url) {
       return get_site_url(1) . '/api/lti/' . $blog_id . '?page_title=chapter%%2F%s';
-    }else{
+    }
+    else {
       return get_site_url(1) . '/api/lti/' . $blog_id . '?page_id=%s';
     }
   }
 
-  private function create_launch_url($page){
-    if( $this->options['use_custom_vars'] ) {
+  private function create_launch_url($page) {
+    if ($this->options['use_custom_vars']) {
       return $this->get_base_url();
-    } else if ( $this->use_page_name_launch_url ) {
+    }
+    else if ($this->use_page_name_launch_url) {
       return sprintf($this->get_base_url(), $page['post_name']);
-    }else{
+    }
+    else {
       return sprintf($this->get_base_url(), $page['ID']);
     }
   }
 
-  private function lti_resources(){
+  private function lti_resources() {
     $resources = '';
-
     $template = <<<XML
 
         <resource identifier="%s" type="imsbasiclti_xmlv1p0">%s
@@ -162,23 +162,20 @@ XML;
 XML;
 
     foreach ($this->book_structure['part'] as $part) {
-      if($this->options['include_parts']) {
-          $resources .= sprintf($template, $this->identifier($part), $this->guid_xml($part), $this->lti_resource_helper($part));
-
+      if ($this->options['include_parts']) {
+        $resources .= sprintf($template, $this->identifier($part), $this->guid_xml($part), $this->lti_resource_helper($part));
       }
-
       foreach ($part['chapters'] as $chapter) {
-        if($this->export_page($chapter)){
-            $resources .= sprintf($template, $this->identifier($chapter), $this->guid_xml($chapter), $this->lti_resource_helper($chapter));
+        if ($this->export_page($chapter)) {
+          $resources .= sprintf($template, $this->identifier($chapter), $this->guid_xml($chapter), $this->lti_resource_helper($chapter));
         }
       }
     }
-
     return $resources;
   }
 
-  private function guid_xml($page){
-    if($this->options['include_guids'] && !empty($this->get_guids($page))){
+  private function guid_xml($page) {
+    if ($this->options['include_guids'] && !empty($this->get_guids($page))) {
       $template = <<<XML
 
             <metadata>
@@ -197,16 +194,16 @@ XML;
     }
   }
 
-  private function lti_resource_helper($page){
-    if(!$this->options['inline']){
+  private function lti_resource_helper($page) {
+    if(!$this->options['inline']) {
       return '<file href="' . $page['ID'] . '.xml"/>';
     }
-    else if($this->options['inline']){
+    else if($this->options['inline']) {
       return $this->link_xml($page);
     }
   }
 
-  private function guids_xml($page){
+  private function guids_xml($page) {
     $guids = '';
     $guids_array = array($this->get_guids($page));
     $template = <<<XML
@@ -215,85 +212,81 @@ XML;
                     </labelledGUID>
 XML;
 
-    if(!empty($guids_array)){
-      foreach ($guids_array as $data){
+    if (!empty($guids_array)) {
+      foreach ($guids_array as $data) {
         foreach ($data as $guid){
           $guids .= sprintf("\n" . $template, $guid);
         }
       }
-
       return $guids;
-    } else {
-      return "";
+    }
+    else {
+      return '';
     }
   }
 
-  private function get_guids($page){
-    if($this->guids_cache === null){
+  private function get_guids($page) {
+    if ($this->guids_cache === null) {
       global $wpdb;
       $sql = $wpdb->prepare( "SELECT post_id, meta_value FROM {$wpdb->postmeta} WHERE meta_key = %s", 'CANDELA_OUTCOMES_GUID' );
 
-      foreach ( $wpdb->get_results( $sql, ARRAY_A ) as $val ) {
-        // Strips whitespace from meta_value
+      foreach ($wpdb->get_results( $sql, ARRAY_A ) as $val) {
         $val['meta_value'] = str_replace(' ', '', $val['meta_value']);
-        // creates an array of values from a string split on comma
         $this->guids_cache[$val['post_id']] = explode(",", $val['meta_value']);
       }
     }
-
     return $this->guids_cache[$page['ID']] ? $this->guids_cache[$page['ID']] : [];
   }
 
-  private function add_lti_link_files($zip){
+  private function add_lti_link_files($zip) {
     foreach ($this->book_structure['part'] as $part) {
-      if($this->options['include_parts']) {
+      if ($this->options['include_parts']) {
         $zip->addFromString($this->identifier($part) . '.xml', $this->link_xml($part, true));
       }
       foreach ($part['chapters'] as $chapter) {
-        if($this->export_page($chapter)) {
+        if ($this->export_page($chapter)) {
           $zip->addFromString($this->identifier($chapter) . '.xml', $this->link_xml($chapter, true));
         }
       }
     }
   }
 
-  private function link_xml($page, $add_xml_header=false){
+  private function link_xml($page, $add_xml_header=false) {
     $launch_url = $this->create_launch_url($page);
     $template = "\n" . $this->link_template;
 
-    if($add_xml_header){
+    if ($add_xml_header) {
       $template = '<?xml version="1.0" encoding="UTF-8"?>' . $template;
     }
 
-    if( $this->options['use_custom_vars'] ){
+    if ($this->options['use_custom_vars']) {
       $custom_variables = '<blti:custom><lticm:property name="page_id">' . $page['ID'] . '</lticm:property></blti:custom>';
-    } else {
+    }
+    else {
       $custom_variables = '';
     }
-
     return sprintf($template, $page['post_title'], $launch_url, $custom_variables);
   }
 
-  private function export_page($page){
+  private function export_page($page) {
     if ($this->options['export_flagged_only']) {
       return $page['export'] == '1';
-    } else {
+    }
+    else {
       return true;
     }
   }
 
-  public function __toString()
-  {
+  public function __toString() {
     return $this->manifest;
   }
 
-  private function get_manifest_template(){
+  private function get_manifest_template() {
     return file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . plugin_basename(self::$templates[$this->version]['manifest']));
   }
 
-  private function get_lti_link_template(){
+  private function get_lti_link_template() {
     return file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . plugin_basename(self::$templates[$this->version]['lti_link']));
   }
-
 }
 ?>
