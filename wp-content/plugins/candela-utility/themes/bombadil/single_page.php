ac3f4c4485d00bbbe605c65a179ed87407937b38
Kelly de Vries <kdv@me.com>
2015-12-02 14:10:36 -0800

    add options for customizing page appearance in nav tab
    Test:
    1. for any book using the bombadil theme, select appearance->theme options
    2. choose navigation options, then enable search bar and part title
    3. view page in book and add ?content_only to end of url
    4. page should display both options
    5. go back to navigation options and disable search bar and part title
    6. view page again with ?content_only
    7. page should no longer display options or single bottom border line
    8. check and uncheck different combinations to make sure works as expected

--- wp-content/plugins/candela-utility/themes/bombadil/single_page.php
+++ wp-content/plugins/candela-utility/themes/bombadil/single_page.php
@@ -39,10 +39,11 @@
 	<?php endif; ?>
 
 	<!-- EDIT PAGE BUTTON -->
+	<?php if (show_edit_button()) { ?>
     <?php edit_post_link(__('Edit This Page', 'lumen'), '<div class="edit-page-btn">', '</div>'); ?>
-
+	<?php } ?>
 	<!-- PAGE NAVIGATION BUTTONS -->
-	<?php if(!isset($_GET['content_only'])) {
+	<?php if(show_navigation_buttons()) {
     ca_get_links();
   } ?>
 
