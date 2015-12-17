db7e2468f7886f01d5dd9f0ba666fcc827980934
monkecheese <bryan.borgeson@gmail.com>
2015-12-16 17:19:14 -0800

    clean up and create functionality for inserting rows into the database when a user navigates to a candela page

--- wp-content/plugins/candela-lti-table-create/candela-lti-table-create.php
+++ wp-content/plugins/candela-lti-table-create/candela-lti-table-create.php
@@ -10,14 +10,17 @@
  * Plugin Github URI: https://github.com/lumenlearning/candela-lti-table-create
  */
 
-function lti_table_activate()
+// If file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function create_db_table()
 {
   // globalize wpdb && charset_collate so that it's accessible
   global $wpdb;
   global $charset_collate;
 
-  // name the table using wp prefix best practice ($table_name = 'wp_bdetector')
-  $table_name = $wpdb->prefix . "lti_table";
+  // name the table using wp prefix best practice ($table_name = 'wp_lti_table')
+  $table_name = $wpdb->prefix . 'lti_table';
 
   // test to see if the db table exists...
   // if it doesn't, make the db table
@@ -25,10 +28,12 @@ function lti_table_activate()
   {
     $sql = 'CREATE TABLE ' . $table_name . '(
     id INTEGER(10) UNSIGNED AUTO_INCREMENT,
-    hit_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER(10),
-    context_id INTEGER(10),
-    account_id INTEGER(10),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    lti_user_id TEXT(100),
+    lti_course_id TEXT(100),
+    lti_account_id TEXT(100),
+    blog_id TEXT(100),
+    page_id TEXT(100),
     PRIMARY KEY (id) )';
 
     // include dbDelta(), which compares the current table structure to the
@@ -41,6 +46,46 @@ function lti_table_activate()
   }
 }
 
-register_activation_hook(__FILE__, 'lti_table_activate');
+register_activation_hook(__FILE__, 'create_db_table');
+
+
+// Start session
+add_action( 'init', 'my_session' );
+function my_session()
+{
+  global $wp_session;
+
+  $wp_session = array(
+    'user_id'    => 'user-id-123',
+    'context_id' => 'context-id-456',
+    'account_id' => 'account-id-789'
+  );
+}
+
+// Insert row into db table when user navigates to page
+add_action( 'wp_loaded', 'insert_row' );
+function insert_row()
+{
+
+  if (!is_admin()) {
+    global $wp_session;
+
+    if (isset($wp_session['user_id'])) {
+      global $wpdb;
+
+      $table_name = "wp_lti_table";
+      $data = array(
+        'timestamp' => current_time('mysql'),
+        'lti_user_id' => '4',
+        'lti_course_id' => '4',
+        'lti_account_id' => '4',
+        'blog_id' => '1',
+        'page_id' => '2'
+      );
+
+      $wpdb->insert( $table_name, $data );
+    }
+  }
+}
 
 ?>
