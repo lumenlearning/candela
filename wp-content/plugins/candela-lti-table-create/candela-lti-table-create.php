8d1f60f83942ec00c622d6f336acf4774ec56758
monkecheese <bryan.borgeson@gmail.com>
2015-12-23 15:51:26 -0800

    grab current wordpress user id and have it stored under lti_user_id in db table.  this is not the correct id but haven't yet found a solution to grab the lti user id

--- wp-content/plugins/candela-lti-table-create/candela-lti-table-create.php
+++ wp-content/plugins/candela-lti-table-create/candela-lti-table-create.php
@@ -13,79 +13,82 @@
 // If file is called directly, abort.
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function create_db_table()
+Candela_LTI_Table_Create::init();
+
+class Candela_LTI_Table_Create
 {
-  // globalize wpdb && charset_collate so that it's accessible
-  global $wpdb;
-  global $charset_collate;
-
-  // name the table using wp prefix best practice ($table_name = 'wp_lti_table')
-  $table_name = $wpdb->prefix . 'lti_table';
-
-  // test to see if the db table exists...
-  // if it doesn't, make the db table
-  if ( $wpdb->get_var('SHOW TABLES LIKE ' . $table_name) != $table_name )
+  public static function init()
   {
-    $sql = 'CREATE TABLE ' . $table_name . '(
-    id INTEGER(10) UNSIGNED AUTO_INCREMENT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    lti_user_id TEXT(100),
-    lti_course_id TEXT(100),
-    lti_account_id TEXT(100),
-    blog_id TEXT(100),
-    page_id TEXT(100),
-    PRIMARY KEY (id) )';
+    register_activation_hook(__FILE__, array(__CLASS__, 'create_db_table' ));
 
-    // include dbDelta(), which compares the current table structure to the
-    // desired table structure, and adds/modifies as necessary
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-
-    // record db table version number
-    add_option('lti_table_database_version', '1.0');
+    add_action( 'init', array( __CLASS__, 'create_db_table' ) );
+    add_action( 'init', array( __CLASS__, 'my_session' ) );
+    add_action( 'wp_loaded', array( __CLASS__, 'insert_row' ) );
   }
-}
 
-register_activation_hook(__FILE__, 'create_db_table');
+  public static function create_db_table()
+  {
+    global $wpdb;
+    global $charset_collate;
 
+    $table_name = $wpdb->prefix . 'lti_table';
 
-// Start session
-add_action( 'init', 'my_session' );
-function my_session()
-{
-  global $wp_session;
+    if ( $wpdb->get_var('SHOW TABLES LIKE ' . $table_name) != $table_name )
+    {
+      $sql = "CREATE TABLE " . $table_name . "(
+      id INTEGER(10) UNSIGNED AUTO_INCREMENT,
+      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      lti_user_id TEXT(10),
+      lti_course_id TEXT(10),
+      lti_account_id TEXT(10),
+      blog_id TEXT(10),
+      page_id TEXT(10),
+      PRIMARY KEY (id) )";
 
-  $wp_session = array(
-    'user_id'    => 'user-id-123',
-    'context_id' => 'context-id-456',
-    'account_id' => 'account-id-789'
-  );
-}
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      dbDelta( $sql );
 
-// Insert row into db table when user navigates to page
-add_action( 'wp_loaded', 'insert_row' );
-function insert_row()
-{
-
-  if (!is_admin()) {
-    global $wp_session;
-
-    if (isset($wp_session['user_id'])) {
-      global $wpdb;
-
-      $table_name = "wp_lti_table";
-      $data = array(
-        'timestamp' => current_time('mysql'),
-        'lti_user_id' => '4',
-        'lti_course_id' => '4',
-        'lti_account_id' => '4',
-        'blog_id' => '1',
-        'page_id' => '2'
-      );
-
-      $wpdb->insert( $table_name, $data );
+      add_option('lti_table_database_version', '1.0');
     }
   }
+
+  public static function my_session()
+  {
+    global $wp_session;
+
+
+    $wp_session = array(
+      'user_id'    => 'user-id-123',
+      'context_id' => 'context-id-456',
+      'account_id' => 'account-id-789'
+    );
+  }
+
+  public static function insert_row()
+  {
+    if (!is_admin()) {
+      global $wp_session;
+
+      if (isset($wp_session['user_id'])) {
+        global $wpdb;
+
+        $table_name = "wp_lti_table";
+        $current_user = wp_get_current_user();
+
+        $data = array(
+          'timestamp' => current_time( 'mysql' ), // this gives a timestamp that is 8 hours ahead
+          'lti_user_id' => $current_user->ID, // this is only wp user id...must get lti user id somehow
+          'lti_course_id' => '4',
+          'lti_account_id' => '4',
+          'blog_id' => '1',
+          'page_id' => '2'
+        );
+
+        $wpdb->insert( $table_name, $data );
+      }
+    }
+  }
+
 }
 
 ?>
