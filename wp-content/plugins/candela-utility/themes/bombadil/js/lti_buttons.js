498d84535c9cdd2b63f75d7ddde100c734bcfdfb
Bracken Mosbacker <bracken@lumenlearning.com>
2016-03-26 17:23:08 -0700

    hide lti navigation buttons if not in an iframe

--- wp-content/plugins/candela-utility/themes/bombadil/js/lti_buttons.js
+++ wp-content/plugins/candela-utility/themes/bombadil/js/lti_buttons.js
@@ -2,23 +2,27 @@
  * LTI Navigation button functionality within LMS
  */
 
-jQuery("#lti-prev").click(function () {
-    parent.postMessage(JSON.stringify({
-        subject: "lti.navigation",
-        location: "previous"
-    }), "*");
-});
+if (self == top){
+    jQuery(".lti-bottom-nav-buttons").hide();
+} else {
+    jQuery("#lti-prev").click(function () {
+        parent.postMessage(JSON.stringify({
+            subject: "lti.navigation",
+            location: "previous"
+        }), "*");
+    });
 
-jQuery("#study-plan").click(function () {
-    parent.postMessage(JSON.stringify({
-        subject: "lti.navigation",
-        location: "home"
-    }), "*");
-});
+    jQuery("#study-plan").click(function () {
+        parent.postMessage(JSON.stringify({
+            subject: "lti.navigation",
+            location: "home"
+        }), "*");
+    });
 
-jQuery("#lti-next").click(function () {
-    parent.postMessage(JSON.stringify({
-        subject: "lti.navigation",
-        location: "next"
-    }), "*");
-});
+    jQuery("#lti-next").click(function () {
+        parent.postMessage(JSON.stringify({
+            subject: "lti.navigation",
+            location: "next"
+        }), "*");
+    });
+}
